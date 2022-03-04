609822f86fce851933581db712cf53dc54580e54
Alex <alexmendes74@gmail.com>
2022-03-04 11:11:39 +0100

    écriture de la classe EtapeIG

--- src/twisk/mondeIG/EtapeIG.java
+++ src/twisk/mondeIG/EtapeIG.java
@@ -1,4 +1,15 @@
 package twisk.mondeIG;
 
 public class EtapeIG {
+    private String nom;
+    private String identifiant;
+    private int posX,posY;
+    private int largeur,hauteur;
+
+    public EtapeIG(String nom,String idf, int larg, int haut){
+        this.nom = nom;
+        this.identifiant = idf;
+        this.largeur = larg;
+        this.hauteur = haut;
+    }
 }
