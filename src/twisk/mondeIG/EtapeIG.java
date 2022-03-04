cad2caa097f5af6b7975d494be3a293ef63e96b1
Alex <alexmendes74@gmail.com>
2022-03-04 13:42:39 +0100

    Ecriture du MondeIG

--- src/twisk/mondeIG/EtapeIG.java
+++ src/twisk/mondeIG/EtapeIG.java
@@ -1,5 +1,7 @@
 package twisk.mondeIG;
 
+import java.util.Random;
+
 public abstract class EtapeIG {
     protected String nom;
     protected String identifiant;
@@ -7,6 +9,10 @@ public abstract class EtapeIG {
     protected int largeur,hauteur;
 
     public EtapeIG(String nom,String idf, int larg, int haut){
+        //on positionne l'étape aléatoirement
+        Random random = new Random();
+        this.posX = 100+random.nextInt(600-100);
+        this.posY = 100+random.nextInt(600-100);
         this.nom = nom;
         this.identifiant = idf;
         this.largeur = larg;
--- src/twisk/mondeIG/MondeIG.java
+++ src/twisk/mondeIG/MondeIG.java
@@ -1,13 +1,18 @@
 package twisk.mondeIG;
 
+import java.util.HashMap;
+
 public class MondeIG {
+    HashMap<Integer, EtapeIG> tableEtape = new HashMap<>() ;
     public MondeIG(){
+        ActiviteIG act = new ActiviteIG("Toboggan","1",10,10);
+        this.tableEtape.put(1,act);
     }
     public void ajouter(String type){
 
     }
-    public void ajouterObservateur(Observateur v){
+    /*public void ajouterObservateur(Observateur v){
 
     }
-    public void notifierObservateurs{}
+    public void notifierObservateurs{}*/
 }
