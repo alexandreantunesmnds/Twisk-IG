051ad13f847661661c000ada5db32c007ee9bf1f
Alex <alexmendes74@gmail.com>
2022-03-06 18:01:09 +0100

    Affichage sortie standard + ajout ToolTip

--- src/twisk/vues/EcouteurBouton.java
+++ src/twisk/vues/EcouteurBouton.java
@@ -2,11 +2,18 @@ package twisk.vues;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import twisk.mondeIG.MondeIG;
 
 public class EcouteurBouton implements EventHandler<ActionEvent> {
+    private MondeIG monde;
+    public EcouteurBouton (MondeIG monde){
+        this.monde = monde;
+    }
 
     @Override
     public void handle(ActionEvent actionEvent) {
+        this.monde.ajouter("Activité");
+        System.out.println("On ajoute une étape");
 
     }
 }
--- src/twisk/vues/VueOutils.java
+++ src/twisk/vues/VueOutils.java
@@ -2,6 +2,7 @@ package twisk.vues;
 
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.TilePane;
@@ -13,12 +14,14 @@ public class VueOutils extends TilePane implements Observateur{
     Button bouton;
     public VueOutils(MondeIG monde){
         this.bouton = new Button();
+        Tooltip tooltip = new Tooltip("Ajouter une étape");
+        Tooltip.install(bouton,tooltip);
         //Creating a graphic (image)
         Image img = new Image("/twisk/ressources/images/button_add.png");
         ImageView view = new ImageView(img);
         this.bouton.setGraphic(view);
         this.getChildren().add(bouton);
-        this.bouton.setOnAction(new EcouteurBouton());
+        this.bouton.setOnAction(new EcouteurBouton(monde));
 
     }
     public void reagir(){
