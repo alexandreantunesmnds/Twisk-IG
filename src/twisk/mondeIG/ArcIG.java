d5882c31aa4259e7d2d5a587f66d3a8f2bb673ac
Alex <alexmendes74@gmail.com>
2022-03-18 09:04:35 +0100

    Test de ArcIG

--- src/twisk/mondeIG/ArcIG.java
+++ src/twisk/mondeIG/ArcIG.java
@@ -1,4 +1,18 @@
 package twisk.mondeIG;
 
 public class ArcIG {
+    PointDeControleIG point1;
+    PointDeControleIG point2;
+    public ArcIG(PointDeControleIG point1, PointDeControleIG point2){
+        this.point1 = point1;
+        this.point2 = point2;
+    }
+
+    public PointDeControleIG getPoint1() {
+        return point1;
+    }
+
+    public PointDeControleIG getPoint2() {
+        return point2;
+    }
 }
