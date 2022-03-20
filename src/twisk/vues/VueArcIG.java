f5eb4e4c50a8081f7f870c693ab911da52f9fda6
Alex <alexmendes74@gmail.com>
2022-03-20 18:21:35 +0100

    Affichage de arrowhead en cours

--- src/twisk/vues/VueArcIG.java
+++ src/twisk/vues/VueArcIG.java
@@ -9,14 +9,17 @@ import twisk.mondeIG.ArcIG;
 public class VueArcIG extends Pane implements Observateur {
     public VueArcIG(ArcIG arc){
         Line line = new Line();
+        // points
+        /*double points[] = { 100.0d, 20.0d, 40.0d, 240.0d, 60.0d,
+                180.0d, 80.0d, 200.0d, 100.0d, 90.0d, 20.0d, 20.0d };*/
         line.setStartX(arc.getPoint1().getPosX());
         line.setStartY(arc.getPoint1().getPosY());
         line.setEndX(arc.getPoint2().getPosX());
         line.setEndY(arc.getPoint2().getPosY());
-        line.setStrokeWidth(10);
-        line.setStroke(Color.PINK);
-        //Polyline polyLine = new Polyline();
-        this.getChildren().add(line);
+        line.setStrokeWidth(4);
+        line.setStroke(Color.MAGENTA);
+        //Polyline polyLine = new Polyline(points);
+        this.getChildren().addAll(line);
     }
 
     @Override
