763a91233534843ceba5709e2e510fd540e611ca
Alex <alexmendes74@gmail.com>
2022-03-31 22:02:45 +0200

    DragAndDrop en cours

--- src/twisk/vues/EcouteurSources.java
+++ src/twisk/vues/EcouteurSources.java
@@ -2,14 +2,31 @@ package twisk.vues;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.input.DragEvent;
+import javafx.scene.input.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import jdk.jfr.Label;
+import twisk.mondeIG.EtapeIG;
 
-public class EcouteurSources implements EventHandler<DragEvent> {
-    public EcouteurSources(){
-
+public class EcouteurSources implements EventHandler<MouseEvent> {
+    private EtapeIG etape;
+    private HBox box;
+    public EcouteurSources(HBox box){
+        this.box = box;
     }
-    @Override
-    public void handle(DragEvent dragEvent) {
 
+    @Override
+    public void handle(MouseEvent t) {
+        System.out.println("DnD detecté.");
+        Circle circle = new Circle();
+        final Dragboard dragBroard = this.box.startDragAndDrop(TransferMode.COPY);
+        // Remlissage du contenu.
+        final ClipboardContent content = new ClipboardContent();
+        // Exporter en tant que texte.
+        content.putString("Un rectangle rouge.");
+        //
+        dragBroard.setContent(content);
+        t.consume();
     }
 }
